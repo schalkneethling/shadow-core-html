96d7a45554e81e5ac976505b28cbf34ca46c9db8
Schalk Neethling <schalkneethling@Schalks-MacBook-Pro.local>
2024-12-05 23:18:39 +0200

    js-step001

---
+++ js/nimbus-team.js
@@ -0,0 +1,25 @@
+class NimbusTeam extends HTMLElement {
+  static #selectors = {
+    UserCardTmpl: "#nimbus-team-user-card-tmpl",
+  };
+
+  #shadow;
+
+  constructor() {
+    super();
+    this.#shadow = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const template = document.getElementById(
+      NimbusTeam.#selectors.UserCardTmpl
+    );
+
+    if (template) {
+      const content = template.content.cloneNode(true);
+      this.#shadow.appendChild(content);
+    }
+  }
+}
+
+customElements.define("nimbus-team", NimbusTeam);
